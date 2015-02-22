878df73c59e66453000e127e6df1e863e6be4dd5
bill-parks <bill_parks@att.net>
2015-02-21 19:50:46 -0500

    Player moves with arrows and cannot move off screen

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(id) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 100;
+    this.y = 100;
+    this.name = "enemy" + id;
+    console.log('Enemy instance created ' + this.name);
 }
 
 // Update the enemy's position, required method for game
@@ -24,12 +28,41 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our player, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+    this.x = 310;
+    this.y = 410;
+    console.log('Player instance created');
+}
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+}
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 
+
+// Place all enemy objects in an array called allEnemies
+var allEnemies = [];
+var addEnemy = new Enemy(1);
+    allEnemies.push(addEnemy);
+
+// Place the player object in a variable called player
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
@@ -44,3 +77,29 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// Player.handleInput() method. Move 10 at a time and stay in bounds.
+Player.prototype.handleInput = function(key) {
+switch(key) {
+  case 'left':
+        if (this.x > 0) {
+           this.x = this.x - 10;
+        };
+        break;
+  case 'right':
+        if (this.x < 410) {
+           this.x = this.x + 10;
+        };
+        break;
+  case 'up':
+       if (this.y > 0){
+            this.y = this.y - 10;
+        };
+       break;
+  case 'down':
+        if (this.y < 430) {
+           this.y = this.y + 10;
+        };
+        break;
+    }
+}
